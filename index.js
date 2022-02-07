6429be96de0cf95130b465d9b3f58f891893aea3
mercy.waeni <mercy.waeni@calltronix.com>
2022-02-07 17:42:20 +0300

    complete static methods lab

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+  static sanitize(word){
+    return word.replace(/[^A-Za-z0-9-\s']+/g, '');
+  }
+  static titleize(word){
+    const allWords = []
+    const immutantWords = ['the','a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    // console.log(word)
+    for (let aword of word.split(' ')){
+      // console.log(aword)
+      if(immutantWords.includes(aword) && word[0]!== aword){
+        // console.log(`${aword} exists in immutables`)
+        allWords.push(aword)
+      }
+      else{
+        // console.log(`${aword} does not exist in immutables`)
+        // console.log(`${this.sanitize(aword)} is my sanitized word`)
+        const capsWord = this.capitalize(aword)
+        // console.log(capsWord)
+        allWords.push(capsWord) 
+      }
+    }
+    console.log(allWords)
+    return allWords.join(' ')
+  }
 }
